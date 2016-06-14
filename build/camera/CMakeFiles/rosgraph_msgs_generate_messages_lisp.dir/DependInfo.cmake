d21b8f356528fd6ae5f2e08bf0daa375bed6beb2
Joe Student <andrewgmorris10@gmail.com>
2016-06-14 11:26:30 -0400

    made odom and drive stuffs

--- build/camera/CMakeFiles/rosgraph_msgs_generate_messages_lisp.dir/DependInfo.cmake
+++ build/camera/CMakeFiles/rosgraph_msgs_generate_messages_lisp.dir/DependInfo.cmake
@@ -16,6 +16,9 @@ SET(CMAKE_TARGET_LINKED_INFO_FILES
 
 # The include file search paths:
 SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/student/watergun_2016/devel/include"
+  "/usr/include/opencv"
+  "/home/student/Downloads/dlib-18.18/dlib/.."
   "/opt/ros/indigo/include"
   )
 SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/drive/CMakeFiles/odom.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/odom.dir/src/odom.cpp.o"
+  "/home/student/watergun_2016/devel/lib/drive/odom.pdb"
+  "/home/student/watergun_2016/devel/lib/drive/odom"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/odom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
