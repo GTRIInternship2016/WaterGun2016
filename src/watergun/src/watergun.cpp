f8f173dc339aacabe7a8a5181069f3dbbe62603d
JoshuaAD <Bell-Inequaliy@users.noreply.github.com>
2016-07-05 08:46:08 -0400

    Commented and Documented

--- src/watergun/src/watergun.cpp
+++ src/watergun/src/watergun.cpp
@@ -1,11 +1,17 @@
+/*
+Simple class that takes target inputs and converts them into neccessary Arduino inputs (degrees for the servo and a shoot/don't shoot bool.)
+*/
+
 #include <ros/ros.h>
 #include <camera/Target.h>
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Bool.h>
 #include <math.h>
 
+// Publishers for the different fields
 ros::Publisher angle_pub, doshoot_pub;
 
+// Called every time a new target is received so that the new Arduino fields are publishes
 void targetCallback(const camera::Target::ConstPtr& msg)
 {
     std_msgs::UInt16 angle;
@@ -15,7 +21,8 @@ void targetCallback(const camera::Target::ConstPtr& msg)
     angle.data = atan(msg->y/msg->z);
     //Publishes each time target is a certain distance away
     doshoot.data = msg->z < 10;
-
+    
+    // Publish the data upon generation. Publishers are put here so that the looping nature of the callback minimizes the need for user created while loops
     angle_pub.publish(angle);
     doshoot_pub.publish(doshoot);
 
@@ -25,10 +32,15 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "watergun");
     ros::NodeHandle n;
-    angle_pub = n.advertise<std_msgs::UInt16>("angle", 100);
+    
+    // Subscribe to the target topipc
     ros::Subscriber target_sub = n.subscribe("target", 100, targetCallback);
+    
+    // Create our publishers
+    angle_pub = n.advertise<std_msgs::UInt16>("angle", 100);
     doshoot_pub = n.advertise<std_msgs::Bool>("doshoot", 100);
 
+    // Spin
     ros::spin();
     return 0;
 }
