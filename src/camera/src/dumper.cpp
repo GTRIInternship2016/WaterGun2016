bee0dfe1109d76c8dc8e133df7579df64f5bf068
JoshuaAD <Bell-Inequaliy@users.noreply.github.com>
2016-07-05 10:13:12 -0400

    Commented and Documented

--- src/camera/src/dumper.cpp
+++ src/camera/src/dumper.cpp
@@ -1,3 +1,6 @@
+/*
+This node takes camera input as an individual image for processing.
+*/
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -6,12 +9,14 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-
+int main(int argc, char** argv){
   ros::init(argc, argv, "dumper");
+  
+  // Create our node handle and then transform it into an image transport node handle
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
+  
+  // Advertise the image
   image_transport::Publisher pub = it.advertise("image_raw", 1);
 
   cv::VideoCapture cap(0);
@@ -20,15 +25,29 @@ int main(int argc, char** argv)
       cout << "Error bra" << endl;
       return 1;
   }
+  
+  // Set the resolution of the image
   cap.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
   cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
+  
+  // Create the cv Mat (image)
   cv::Mat frame;
+  
+  // Create the image to be forwarded
   sensor_msgs::ImagePtr msg;
+  
+  // While the NodeHandle is working...
   while (nh.ok()) {
+    // Feed the capture into the Mat
     cap >> frame;
     // Check if grabbed frame is actually full with some content
+    
+    // As logn as the frame contains data...
     if(!frame.empty()) {
+      // Convert the cv Mat to an ImagePtr message
       msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+      
+      // Then publish!
       pub.publish(msg);
       //cv::waitKey(1);
     }
